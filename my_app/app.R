cc74c2086111460a9efaf4806b9c4a3e60a4c538
Alejandro Dazun <dazunalejandro@gmail.com>
2019-03-06 13:56:59 +0100

    -select input control sent by the server via `updateSelectInput -Make Refereces collapsable

--- my_app/app.R
+++ my_app/app.R
@@ -10,38 +10,55 @@
 library(shiny)
 library(tidyverse)
 
-
 list_choices <-  unique(msleep$vore)[!is.na(unique(msleep$vore))]
 names(list_choices) <- paste(unique(msleep$vore)[!is.na(unique(msleep$vore))],"vore",sep="")
 
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("This is a new Shiny app"),
-   
-   
-   includeMarkdown("references.md"),
-   
-   selectInput("select", label = h3("Plot by type of alimentation"), 
-               choices = list_choices,
-               selected = 1),
-   
-   plotOutput(outputId = "plot")
-   
+# Define UI for application that draws a histogram
+ui <- fluidPage( 
+  
+  p(tags$button(class="btn btn-default", 
+                `data-toggle`="collapse", 
+                `data-target`="#collapseExample",
+                "References")),
+  
+  div(class="collapse", id="collapseExample",
+      div(class="card card-body",
+          includeMarkdown("references.md")
+      )),
+  
+  # Application title
+  titlePanel("This is a new Shiny app"),
+  
+  includeMarkdown("references.md"),
+  
+  selectInput("select", label = h3("Plot by type of alimentation"), 
+              choices = character(0),
+              selected = 1),
+  
+  plotOutput(outputId = "plot")
+  
 )
 
 col_scale <- scale_colour_discrete(limits = unique(msleep$vore))
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
-   
+server <- function(input, output, session) {
+  
+  # Can also set the label and select items
+  updateSelectInput(session, "select",
+                    choices = list_choices,
+                    selected = tail(list_choices, 1)
+  )
+  
   output$plot <- renderPlot({
-    ggplot(msleep %>% filter(vore == input$select), aes(bodywt, sleep_total, colour = vore)) +
-      scale_x_log10() +
-      col_scale +
-      geom_point()
+    # cat(file=stderr(), "input$select:", input$select == "", "\n")
+    if(input$select != ""){
+      ggplot(msleep %>% filter(vore == input$select), aes(bodywt, sleep_total, colour = vore)) +
+        scale_x_log10() +
+        col_scale +
+        geom_point()
+    }
   })
-
 }
 
 # Run the application 
