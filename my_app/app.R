49d638d49b64e4e5e7e7a8d00a0f120afff87192
Alejandro Dazun <dazunalejandro@gmail.com>
2019-03-06 13:41:51 +0100

    Change the title Created references file included reference file in the same page Add some plots fileter by vore and add select input

--- my_app/app.R
+++ my_app/app.R
@@ -8,41 +8,40 @@
 #
 
 library(shiny)
+library(tidyverse)
 
-# Define UI for application that draws a histogram
+
+list_choices <-  unique(msleep$vore)[!is.na(unique(msleep$vore))]
+names(list_choices) <- paste(unique(msleep$vore)[!is.na(unique(msleep$vore))],"vore",sep="")
+
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Old Faithful Geyser Data"),
+   titlePanel("This is a new Shiny app"),
    
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+   
+   includeMarkdown("references.md"),
+   
+   selectInput("select", label = h3("Plot by type of alimentation"), 
+               choices = list_choices,
+               selected = 1),
+   
+   plotOutput(outputId = "plot")
+   
 )
+
+col_scale <- scale_colour_discrete(limits = unique(msleep$vore))
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+  output$plot <- renderPlot({
+    ggplot(msleep %>% filter(vore == input$select), aes(bodywt, sleep_total, colour = vore)) +
+      scale_x_log10() +
+      col_scale +
+      geom_point()
+  })
+
 }
 
 # Run the application 
